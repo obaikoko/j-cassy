7c49def92ad689cb7e72af505adad0f98b04b5a2
obaikoko <jesseobinna7@gmail.com>
2023-10-19 21:54:46 +0100

    another commit

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://j-cassybackend.onrender.com/api/users/';
-const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = 'https://j-cassybackend.onrender.com/api/users/';
+// const API_URL = 'http://localhost:5000/api/users/';
 
 const register = async (userData) => {
   const response = await axios.post(API_URL + 'register', userData);
